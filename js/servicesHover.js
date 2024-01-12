ef4795c64e17722a84035238e0f739eba62b1f52
Всеволод Прокопов <125273666+ProkopovVsevolod@users.noreply.github.com>
2024-01-12 13:36:46 +0400

    Готовая страница услуг и главная

--- js/servicesHover.js
+++ js/servicesHover.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const sectionElement = document.querySelectorAll("[data-section='mouseHover']");
 
 console.log(sectionElement)
@@ -20,4 +21,28 @@ sectionElement.forEach(section => {
 
 const addBackgroundImg = (section) => {
     
+=======
+const sectionElement = document.querySelectorAll("[data-section='mouseHover']");
+
+console.log(sectionElement)
+sectionElement.forEach(section => {
+
+    section.addEventListener("mouseover", (e) => {
+        e.stopPropagation();
+        section.classList.add("active");
+        console.log(section.parentNode.parentNode.parentNode.style.backgroundColor = "transparent")
+        section.nextSibling.style.backgroundColor = "white";
+        section.previousSibling.style.backgroundColor = "white";
+        // section.style.backgroundImg = "./../../../img/services/bg-1.png";
+
+    });
+
+    section.addEventListener("mouseout", () => {
+        section.classList.remove("active");
+    });
+})
+
+const addBackgroundImg = (section) => {
+    
+>>>>>>> ea19f0a9f0440b69cc6b413c9a19fc7b8dba21be
 }
