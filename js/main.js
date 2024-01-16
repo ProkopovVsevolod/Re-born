b45e7d9f9c550017100f218aaf929a89581e5154
ProkopovVsevolod <vaprokopov@mail.ru>
2024-01-16 18:11:06 +0400

    Добавлено выпадающее меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-<<<<<<< HEAD
 const accordion = document.querySelectorAll("[data-accordion]");
 const anchors = document.querySelectorAll("[data-id='anchorButton']");
+const dropbtn = document.querySelectorAll(".header__dropbtn");
+const dropdownContent = document.querySelector(".header__dropdown-content");
+
+
+console.log(dropbtn)
+dropbtn.forEach(button => {
+    button.addEventListener("mouseenter", (e) => {
+        console.log(e.target)
+        // dropdownContent.classList.add("active");
+        console.log(e.target.children[1].classList.add("active"))
+    })
+});
+
+dropdownContent.addEventListener("mouseleave", (e) => {
+    e.stopPropagation();
+    dropdownContent.classList.remove("active");   
+})
 
 
 
@@ -25,45 +41,4 @@ anchors.forEach(anchor => {
 
 
 
-// const flag = document.querySelector("#flag");
-// const submitBtn = document.querySelector("#submitButton");
 
-
-// if (flag.checked) {
-//     submitBtn.disabled = false;
-// } else { submitBtn.disabled = true;}
-=======
-const accordion = document.querySelectorAll("[data-accordion]");
-const anchors = document.querySelectorAll("[data-id='anchorButton']");
-
-
-
-accordion.forEach(accordionTitle => {
-    accordionTitle.addEventListener("click", () => {
-        accordionTitle.classList.toggle('active');
-        accordionTitle.children[0].classList.toggle("active");
-        accordionTitle.children[1].classList.toggle("active");
-        accordionTitle.lastElementChild.classList.toggle("hidden");
-    })
-})
-
-
-anchors.forEach(anchor => {
-    anchor.addEventListener("click", () => {
-        anchors.forEach(anchor => {
-            anchor.classList.remove("active");
-        })
-        anchor.classList.add('active');
-    })
-})
-
-
-
-// const flag = document.querySelector("#flag");
-// const submitBtn = document.querySelector("#submitButton");
-
-
-// if (flag.checked) {
-//     submitBtn.disabled = false;
-// } else { submitBtn.disabled = true;}
->>>>>>> ea19f0a9f0440b69cc6b413c9a19fc7b8dba21be
--- js/servicesHover.js
+++ js/servicesHover.js
@@ -1,48 +1,24 @@
-<<<<<<< HEAD
-const sectionElement = document.querySelectorAll("[data-section='mouseHover']");
+const heroBlock = document.querySelector('[data-animation="start"]');
+const endBlock = document.querySelector('[data-animation="end"]');
 
-console.log(sectionElement)
-sectionElement.forEach(section => {
+const positionOfEndBlock = endBlock.getBoundingClientRect();
+let rect = heroBlock.getBoundingClientRect();
 
-    section.addEventListener("mouseover", (e) => {
-        e.stopPropagation();
-        section.classList.add("active");
-        console.log(section.parentNode.parentNode.parentNode.style.backgroundColor = "transparent")
-        section.nextSibling.style.backgroundColor = "white";
-        section.previousSibling.style.backgroundColor = "white";
-        // section.style.backgroundImg = "./../../../img/services/bg-1.png";
+const html = `<div class="w50"></div>`
 
-    });
-
-    section.addEventListener("mouseout", () => {
-        section.classList.remove("active");
-    });
-})
-
-const addBackgroundImg = (section) => {
-    
-=======
-const sectionElement = document.querySelectorAll("[data-section='mouseHover']");
-
-console.log(sectionElement)
-sectionElement.forEach(section => {
-
-    section.addEventListener("mouseover", (e) => {
-        e.stopPropagation();
-        section.classList.add("active");
-        console.log(section.parentNode.parentNode.parentNode.style.backgroundColor = "transparent")
-        section.nextSibling.style.backgroundColor = "white";
-        section.previousSibling.style.backgroundColor = "white";
-        // section.style.backgroundImg = "./../../../img/services/bg-1.png";
-
-    });
-
-    section.addEventListener("mouseout", () => {
-        section.classList.remove("active");
-    });
-})
-
-const addBackgroundImg = (section) => {
-    
->>>>>>> ea19f0a9f0440b69cc6b413c9a19fc7b8dba21be
+if (rect.top <= 210 ) {
+    heroBlock.parentNode.insertAdjacentHTML("afterbegin", html);
 }
+
+window.addEventListener('scroll', function() {
+    rect = heroBlock.getBoundingClientRect();
+    console.log(rect.top)
+    if (rect.top = 210) {
+        
+     // Блок находится в области видимости, примените анимацию
+        heroBlock.classList.add('animate');
+    } else if (positionOfEndBlock.top === 10  ) {
+     // Блок не в области видимости, удалите анимацию
+        heroBlock.classList.remove('animate');
+    }
+   });
