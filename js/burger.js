34e89d9322e4c824226f2519100c9cbf2b48686c
ProkopovVsevolod <vaprokopov@mail.ru>
2024-01-17 12:51:19 +0400

    Поправлены баги на всех страницах

--- js/burger.js
+++ js/burger.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const burger = document.querySelector(".header__burger-wrapper");
 
 burger.addEventListener("click", () => {
     document.querySelector('.header__burger').classList.toggle("active");
-=======
-const burger = document.querySelector(".header__burger-wrapper");
-
-burger.addEventListener("click", () => {
-    document.querySelector('.header__burger').classList.toggle("active");
->>>>>>> ea19f0a9f0440b69cc6b413c9a19fc7b8dba21be
 })
