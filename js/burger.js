737b90944a36dcc01754daca22ec3353b1eda30d
ProkopovVsevolod <vaprokopov@mail.ru>
2024-03-17 12:32:39 +0400

    1/2 реализация бургера

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector(".header__burger-wrapper");
+const burgerContent = document.querySelector(".header__burger-nav-wrapper");
+
 
 burger.addEventListener("click", () => {
     document.querySelector('.header__burger').classList.toggle("active");
+    burgerContent.classList.toggle("active")
 })
